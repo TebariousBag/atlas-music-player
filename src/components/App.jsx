59cdbe77c11dbb7899b77ab3da74c3d065986d06
“Tristian <tristian.davis@atlasschool.com>
2025-09-16 14:47:16 -0500

    responiveness was really simple. task 4 is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,10 @@
 import Footer from "./Footer";
 import MusicPlayer from "./MusicPlayer";
-import CurrentlyPlaying from "./CurrentlyPlaying";
-import Playlist from "./Playlist";
 
 function App() {
   return (
     <div className="flex h-full min-h-screen flex-col justify-between p-8">
-      <div className="flex">
-        <div className="w-full p-6">
-          <CurrentlyPlaying />
-        </div>
-        <div className="w-full p-6">
-          <Playlist />
-        </div>
-      </div>
+      <MusicPlayer />
       <Footer />
     </div>
   );
--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,4 +1,15 @@
+import CurrentlyPlaying from "./CurrentlyPlaying";
+import Playlist from "./Playlist";
+
 export default function MusicPlayer() {
-  return <p>Music player goes here</p>
-  ;
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="w-full p-6 md:w-1/2">
+        <CurrentlyPlaying />
+      </div>
+      <div className="w-full p-6 md:w-1/2">
+        <Playlist />
+      </div>
+    </div>
+  );
 }
