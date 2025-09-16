4468fa9ca4d4b76e8435c508f020ab692edb3782
“Tristian <tristian.davis@atlasschool.com>
2025-09-16 12:06:02 -0500

    last component added, task 2 done

---
+++ src/components/PlayListItem.jsx
@@ -0,0 +1,13 @@
+export default function PlayListItem() {
+  return (
+    <div className="flex items-center">
+      <div>
+        <p>Painted in Blue</p>
+        <p className="text-slate-500">Soul Canvas</p>
+      </div>
+      <div>
+        <p>5:55</p>
+      </div>
+    </div>
+  );
+}
