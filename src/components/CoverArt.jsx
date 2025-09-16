2b6ade7cba124fd0cd1d722a190119c6b0660552
“Tristian <tristian.davis@atlasschool.com>
2025-09-16 11:39:11 -0500

    cover art and songtitle component

---
+++ src/components/CoverArt.jsx
@@ -0,0 +1,9 @@
+import placeholderImage from "../assets/placeholder.svg";
+
+export default function CoverArt() {
+  return (
+    <div className="">
+      <img src={placeholderImage} className="" />
+    </div>
+  );
+}
---
+++ src/components/SongTitle.jsx
@@ -0,0 +1,8 @@
+export default function SongTitle() {
+  return (
+    <div className="text-center">
+      <h2 class="">Tidal Drift</h2>
+      <p class="text-slate-500">Echoes of the Sea</p>
+    </div>
+  );
+}
