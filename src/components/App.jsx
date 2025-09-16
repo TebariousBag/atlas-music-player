c3e347565c4dfdb15f46af3ef23764207b8a0f34
“Tristian <tristian.davis@atlasschool.com>
2025-09-16 18:11:20 -0500

    removed loadingskeleton from app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,6 @@ import LoadingSkeleton from "./LoadingSkeleton";
 function App() {
   return (
     <div className="bg-french-blue flex h-full min-h-screen flex-col justify-between p-8 dark:bg-gray-800">
-      <LoadingSkeleton />
       <MusicPlayer />
       <Footer />
     </div>
