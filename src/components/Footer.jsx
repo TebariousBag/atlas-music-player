4a9b1635180a773f4e73264470d6244eea00192a
“Tristian <tristian.davis@atlasschool.com>
2025-09-16 18:15:49 -0500

    needed to give a dark variant to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <div className="text-aero p-8 text-center">&copy; {year} Atlas School</div>
+    <div className="text-aero p-8 text-center dark:text-gray-400">
+      &copy; {year} Atlas School
+    </div>
   );
 }
