c06595f267d75af65dd8f8b458ac24db1b69a62c
“Tristian <tristian.davis@atlasschool.com>
2025-09-16 13:24:35 -0500

    playcontrols looking better, I cant figure out why my icons are different than what the are supposed to be

--- src/components/PlayControls.jsx
+++ src/components/PlayControls.jsx
@@ -2,12 +2,20 @@ import { SkipBack, Pause, Play, SkipForward, Shuffle } from "lucide-react";
 
 export default function PlayControls() {
   return (
-    <div className="flex items-center">
-      <p>1x</p>
-      <SkipBack />
-      <Play />
-      <SkipForward />
-      <Shuffle />
+    <div className="flex w-full items-center justify-between">
+      <p className="mx-2 text-center text-2xl">1x</p>
+      <button className="mx-2 cursor-pointer rounded">
+        <SkipBack />
+      </button>
+      <button className="mx-2 cursor-pointer rounded-xl border-2 p-4">
+        <Play />
+      </button>
+      <button className="mx-2 cursor-pointer rounded">
+        <SkipForward />
+      </button>
+      <button className="mx-2 cursor-pointer rounded">
+        <Shuffle />
+      </button>
     </div>
   );
 }
