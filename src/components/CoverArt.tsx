fec78c23a2d601fe0dc33bec1165c511c323054f
“Tristian <tristian.davis@atlasschool.com>
2025-09-17 14:25:06 -0500

    setting up .tsx step by step. cover art I think is done

--- src/components/CoverArt.tsx
+++ src/components/CoverArt.tsx
@@ -1,10 +1,11 @@
 import placeholderImage from "../assets/placeholder.svg";
+import { CoverArtProps } from "../types";
 
-export default function CoverArt() {
+export default function CoverArt({ selectedSong }: CoverArtProps) {
   return (
     <div className="w-full">
       <img
-        src={placeholderImage}
+        src={selectedSong?.cover || placeholderImage}
         className="border-aero rounded-lg border-4 dark:border-gray-500 dark:bg-gray-600"
       />
     </div>
---
+++ src/types.ts
@@ -0,0 +1,24 @@
+// api response types from provided api
+export interface PlaylistSong {
+  id: string;
+  title: string;
+  artist: string;
+  genre: string;
+  duration: number;
+}
+
+// song extends from playlist giving the cover art and link to song
+export interface Song extends PlaylistSong {
+  cover: string;
+  song: string;
+}
+
+// optional, skipping for now
+export interface Lyrics {
+  lyrics: string;
+}
+
+// compnent prop types
+export interface CoverArtProps {
+  selectedSong?: Song | null;
+}
