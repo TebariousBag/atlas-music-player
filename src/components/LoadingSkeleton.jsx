ceeb122b0399341a000235b6deaaaa1e211f3d0d
“Tristian <tristian.davis@atlasschool.com>
2025-09-23 11:18:48 -0500

    centered up lloading skeleton

--- src/components/LoadingSkeleton.jsx
+++ src/components/LoadingSkeleton.jsx
@@ -3,7 +3,7 @@ import PlayControlsDark from "./PlayControlsDark";
 
 export default function LoadingSkeleton() {
   return (
-    <div className="flex animate-pulse">
+    <div className="flex min-h-screen animate-pulse items-center justify-center">
       {/* Left container */}
       <div className="flex h-[600px] w-[600px] flex-col items-center justify-center bg-gray-50">
         <div>
